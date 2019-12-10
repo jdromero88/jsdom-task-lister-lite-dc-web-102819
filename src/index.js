f9c7d5c7e9cb174240158f9d008f75a8f38ed1c1
jdromero88 <github email address>
2019-12-10 16:13:52 -0500

    delete button added and functioning

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   console.log('do something MF');
   const formEl = document.getElementById('create-task-form')
-  formEl.addEventListener('submit', submitForm) 
+  formEl.addEventListener('submit', submitForm)
 });
 
 
@@ -10,11 +10,32 @@ function submitForm(event) {
   event.preventDefault()
   let description = document.getElementById('new-task-description').value
   addToList(description)
+  event.target.reset()
 }
 
 function addToList(description) {
   let li = document.createElement('li')
-  li.innerText = description
+  li.innerText = description + " ";
+  li.dataset.description = description.split(" ").join("");
+  let removeLi = document.createElement('button')
+  removeLi.innerText = "X"
+  removeLi.dataset.description = description.split(" ").join("");
+  removeLi.addEventListener("click", deleteLi)
+  li.appendChild(removeLi)
   let ulEl = document.querySelector('#tasks')
   ulEl.appendChild(li)
+}
+
+function deleteLi(event){
+  // get element by description
+  y = event.currentTarget.dataset.description
+  x = document.querySelectorAll(`[data-description ~= ${y}]`)
+  x[0].remove();
+  // console.log(x);
+
+  // destroy element
+  // x.remove();
+  // celebrate
+  // don't reload page
+
 }
