168e5eacc3296f743623fef7d97b70a810c97dec
jdromero88 <github email address>
2019-12-10 15:15:37 -0500

    our form is adding the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  console.log('do something MF');
+  const formEl = document.getElementById('create-task-form')
+  formEl.addEventListener('submit', submitForm) 
 });
+
+
+function submitForm(event) {
+  event.preventDefault()
+  let description = document.getElementById('new-task-description').value
+  addToList(description)
+}
+
+function addToList(description) {
+  let li = document.createElement('li')
+  li.innerText = description
+  let ulEl = document.querySelector('#tasks')
+  ulEl.appendChild(li)
+}
